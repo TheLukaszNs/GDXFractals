2993124ae1df889f15e3b80030d22fa1e2b1c497
Łukasz Myśliwiec <lmysliwiec10@gmail.com>
2022-01-22 20:07:53 +0100

    feat: initial commit 🚀

---
+++ desktop/src/com/celestial/onion/fractals/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.celestial.onion.fractals.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.celestial.onion.fractals.GDXFractals;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GDXFractals(), config);
+	}
+}
